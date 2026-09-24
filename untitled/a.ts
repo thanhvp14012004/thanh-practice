32a1d0bc21a613052d6835403ebaa42871f08d69
thanhvp14012004 <thanhvp14012004@gmail.com>
2026-09-24 14:27:09 +0700

    Xóa file a, b và tạo file c

--- untitled/a.ts
+++
@@ -1,2 +0,0 @@
-import {userAdmin} from "./b.ts";
-console.log ("Tên đăng nhập là", userAdmin);
---
+++ untitled/c.ts
@@ -0,0 +1,3 @@
+const productName: string = "Ao dai";
+const price: number = 300;
+console.log(`San pham ${productName} có giá ${price} VND`);
